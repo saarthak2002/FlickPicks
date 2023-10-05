c70844be5265e9cabc012df2652c02117c60c06e
Saarthak Gupta <79454214+saarthak2002@users.noreply.github.com>
2023-10-05 02:18:10 -0400

    added new field to sign up page

--- php/signup.php
+++ php/signup.php
@@ -1,3 +1,9 @@
 <?php
     echo "signup.php";
+    $firstName = $_POST['firstName'];
+    $lastName = $_POST['lastName'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+
+    echo "\n$firstName $lastName $email $password";
 ?>
